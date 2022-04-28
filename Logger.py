772886d4b1137d8b92a00feb5eaa4fc13427ccae
Xander-C <8166568+xander-c@user.noreply.gitee.com>
2022-04-28 22:41:50 +0800

    fix: save to csv with no index

--- Logger.py
+++ Logger.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 def init_logger():
     client_loss_df = pd.DataFrame(columns=['client_id', 'local_epoch', 'loss'])
-    client_loss_df.to_csv("./result/client_loss.csv", mode='w')
+    client_loss_df.to_csv("./result/client_loss.csv", mode='w', index=False)
 
     distill_result_df = pd.DataFrame(columns=['round', 'distill_loss', 'hr', 'hdcg'])
-    distill_result_df.to_csv("./result/distill_result.csv", mode='w')
+    distill_result_df.to_csv("./result/distill_result.csv", mode='w', index=False)
 
 
 def log_client_loss(client_id, local_epoch, loss):
     client_loss_df = pd.DataFrame([[client_id, local_epoch, loss]], columns=['client_id', 'local_epoch', 'loss'])
-    client_loss_df.to_csv("./result/client_loss.csv", mode='a', header=False)
+    client_loss_df.to_csv("./result/client_loss.csv", mode='a', header=False, index=False)
 
 
 def log_distill_result(rnd, distill_loss, hr, hdcg):
     distill_result_df = pd.DataFrame([[rnd, distill_loss, hr, hdcg]], columns=['round', 'distill_loss', 'hr', 'hdcg'])
-    distill_result_df.to_csv("./result/distill_result.csv", mode='a', header=False)
+    distill_result_df.to_csv("./result/distill_result.csv", mode='a', header=False, index=False)
